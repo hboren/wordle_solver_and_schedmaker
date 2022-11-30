e5ce52bf9eee92f8a2284d9cc12642e4de41576f
Haley Boren <hboren@usc.edu>
2022-11-30 07:08:33 +0000

    adding wordle and schedword

--- schedwork.cpp
+++ schedwork.cpp
@@ -19,7 +19,10 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool sched_helper( DailySchedule& sched, int num_days, int d_needed, const AvailabilityMatrix& avail, int n, int d, const size_t maxShifts);
 
+
+bool isValid(DailySchedule& sched, int num_days, int d_needed, const AvailabilityMatrix& avail, const size_t maxShifts);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -35,9 +38,95 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    int n = (int)avail.size();
 
-
-
+    //fill sched with invalid ID in each spot
+    for(int i = 0; i < n; i++){
+        std::vector<std::vector<bool>> AvailabilityMatrix;
+        std::vector<Worker_T> day;
+        for(int j = 0; j < (int)dailyNeed; j++){
+            day.push_back(INVALID_ID);
+        }
+        sched.push_back(day);
+    }
+		//call helper
+		return sched_helper(sched, 0, 0, avail, n, (int)dailyNeed, maxShifts);
 
 }
 
+bool sched_helper(DailySchedule& sched, int num_days, int d_needed, const AvailabilityMatrix& avail, int n, int d, const size_t maxShifts)
+{
+	//if we made a complete schedule, return true, should be valid
+	if(num_days == n -1 && d_needed == d){
+		return true;
+	}
+
+	//if we have gone through the cols, now recurse to next row
+	if(d_needed == d){
+		return sched_helper(sched, num_days+1, 0, avail, n, d, maxShifts);
+	}
+	//if the current place is empty, try each id and check if it is a valid sched
+	//if it is, keep recursing to next spot and if those are valid, return true 
+	//until schedule is full
+	if(sched[num_days][d_needed] == INVALID_ID){
+		for(int v = 0; v < (int)avail[0].size(); v++){
+				sched[num_days][d_needed] = v;
+			if(isValid(sched, num_days, d_needed, avail, maxShifts)){
+				if(sched_helper(sched, num_days, d_needed+1, avail, n, d, maxShifts) == true){
+					return true;
+				}
+			}
+		}
+		//reset changes
+		sched[num_days][d_needed] = INVALID_ID;
+		return false;
+	}
+	//otherwise recurse to next position
+	else{
+			return sched_helper(sched, num_days, d_needed+1, avail, n, d, maxShifts);
+	}
+}
+
+//determines if current state is a valid schedule
+bool isValid(DailySchedule& sched, int num_days, int d_needed, const AvailabilityMatrix& avail, const size_t maxShifts)
+{
+	//get the current value
+	int value = sched[num_days][d_needed];
+
+	//if the person we added to the schedule is not available not valid
+	if(avail[num_days][value] == 0){
+		return false;
+	}
+
+
+	//if they are scheduled multiple times in one day not valid
+	for(int i = 0; i < d_needed; i++){
+		if((int)sched[num_days][i] == value){
+			return false;
+		}
+	}
+
+	int counter = 0;
+
+	int d = sched[0].size();
+	int n = sched.size();
+
+	//count how many times they are scheduled
+	for(int i = 0; i < d; i++){
+		for(int j = 0; j < n; j++){
+			if((int)sched[j][i] == value){
+				counter++;
+			}
+		}
+
+	}
+
+	//if scheduled more than max shifts allows, not valid
+	if(counter > (int)maxShifts){
+		return false;
+	}
+
+	//otherwise should be valid
+	return true;
+	
+}
--- wordle.cpp
+++ wordle.cpp
@@ -19,7 +19,84 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    set<std::string> all_possible;
+    set<string> all_english;
+
+    std::string floating_pass = floating;
+    std::string in_pass = in;
+
+    //call helper
+    wordle_helper(in, floating_pass, all_possible, 0);
+
+    //keep only english words out of all the possible letter combinations
+   for(set<string>::iterator it=all_possible.begin(); it != all_possible.end(); ++it){
+        if(dict.find(*it) != dict.end()){
+            all_english.insert(*it);
+        }
+    }
+
+    return all_english;
 
 }
 
 // Define any helper functions here
+void wordle_helper(
+    std::string in,
+    std::string float_, 
+    std::set<std::string>& result, 
+    int pos)
+{
+
+    //if the word we are making is complete, add it to our results
+    if(pos == (int)in.size()){
+        result.insert(in);
+        return;
+    }
+
+    //if no letter fixed at current position:
+    if(in[pos] == '-'){
+        //loop through the floating letters
+        for(int h = 0; h < (int)float_.size(); h++){
+            string float_temp = float_;
+            //set current position to current floating letter and remove from temp float
+            in[pos] = float_[h];
+            float_temp.erase(h,1);
+            //recurse to next position
+            wordle_helper(in, float_temp, result, pos + 1);
+            //reset changes
+            in[pos] = '-';
+            float_temp = float_;
+        }
+        //if the current position is empty or we have used all floating letters
+        //loop over alphabet, try each letter, if still valid, recurse to next position
+        //then reset
+        if(in[pos] == '-' || float_.size() == 0){
+            for(char j = 'a'; j <= 'z'; j++){
+                    in[pos] = j;
+                //if it is a valid word, aka it will use all yellows
+                    if(is_valid(in, float_)){
+                        wordle_helper(in, float_, result, pos + 1);
+                    }
+                in[pos] = '-';
+            }
+        }
+    }
+    //otherwise just add green letter and continue
+    else{
+        return wordle_helper(in, float_, result, pos + 1);
+    }
+}
+
+//checks if word in current state is a valid word, AKA will be able to use all floats
+bool is_valid(std::string in, std::string float_){
+    int num_open = 0;
+    for(int i = 0; i < (int)in.size(); i++){
+        if(in[i] == '-')
+            num_open++;
+    }
+    //if there are less open letters than floats remaining, not valid, otherwise valid
+    if(num_open < (int)float_.size()){
+        return false;
+    }
+    return true;
+}
